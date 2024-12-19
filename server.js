9d488f0ff2f92407d345229e926bb07a4bebd0bb
Jose <Jose0156.114@gmail.com>
2024-12-18 18:57:59 -0600

    add public

--- server.js
+++ server.js
@@ -14,6 +14,11 @@ app.get("/", (req, res) => {
     res.sendFile(path.join(__dirname, "public", "index.html"));
 });
 
+// Ruta para archivos TypeScript transpilados
+app.get("/dist/*", (req, res) => {
+    res.sendFile(path.join(__dirname, "dist", req.params[0]));
+});
+
 app.listen(PORT, () => {
     console.log(`Server running on http://localhost:${PORT}`);
 });
